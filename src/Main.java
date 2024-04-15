8d2d323d6e1d4d3421a41a27b4883449dbbf41b7
KaykyCajui07 <cajuikayky68@gmail.com>
2024-04-15 13:41:13 -0300

    Atualizacao

--- src/Main.java
+++ src/Main.java
@@ -17,33 +17,33 @@ public class Main {
         int opcao = 0;
 
         Inicio();
-        while (true) {
-            System.out.println("Escolha o processador: ");
-            System.out.println();
-            estoque.catalogoProcessadores();
-            opcao = input.nextInt();
-            pedido.addProcessador(opcao);
+        System.out.println("Escolha o processador: ");
+        System.out.println();
+        estoque.catalogoProcessadores();
+        opcao = input.nextInt();
+        pedido.addProcessador(opcao);
 
-            System.out.println("Escolha Placa Mae:");
-            if (pedido.getProcessador().getMarca() == "AMD") {
+        System.out.println("Escolha Placa Mae:");
+        if (pedido.getProcessador().getMarca() == "AMD") {
                 estoque.catalogoPlacasMaeAMD();
-            } else {
+            }
+        else {
                 estoque.catalogoPlacasMaeIntel();
             }
 
-            opcao = input.nextInt();
-            pedido.addPlacaMae(opcao);
-            System.out.println();
+        opcao = input.nextInt();
+        pedido.addPlacaMae(opcao);
+        System.out.println();
 
-            System.out.println("Escolha a Memoria Ram:");
-            for (int i = 0; i < 2; i++) {
-                if (pedido.getPlacaMae().getNome() == "Asus A620M"
+        System.out.println("Escolha a Memoria Ram:");
+        for (int i = 0; i < 2; i++) {
+            if (pedido.getPlacaMae().getNome() == "Asus A620M"
                         || pedido.getPlacaMae().getNome() == "Gigabyte B760M") {
-                    estoque.catalogoMemoriasRAMDDR5();
-                    opcao = input.nextInt();
-                    pedido.addMemoriaRam(opcao);
+                estoque.catalogoMemoriasRAMDDR5();
+                opcao = input.nextInt();
+                pedido.addMemoriaRam(opcao);
 
-                } else {
+            } else {
                     estoque.catalogoMemoriasRAMDDR4();
                     opcao = input.nextInt();
                     pedido.addMemoriaRam(opcao);
@@ -51,50 +51,50 @@ public class Main {
                 }
             }
             
-            System.out.println("Escolha a Placa De Video");
-            estoque.catalogoPlacasDeVideo();
-            opcao = input.nextInt();
-            pedido.addPlacaVideo(opcao);
+        System.out.println("Escolha a Placa De Video");
+        estoque.catalogoPlacasDeVideo();
+        opcao = input.nextInt();
+        pedido.addPlacaVideo(opcao);
 
-            System.out.println("Escolha o Armazenamento");
-            estoque.catalogoArmazenamentos();
-            opcao = input.nextInt();
-            pedido.addArmazenamento(opcao);
+        System.out.println("Escolha o Armazenamento");
+        estoque.catalogoArmazenamentos();
+        opcao = input.nextInt();
+        pedido.addArmazenamento(opcao);
             
-            System.out.println("Escolha a Fonte De Alimentacao: ");
-            estoque.catalogoFontes();
-            opcao = input.nextInt();
-            pedido.addFonte(opcao);
+        System.out.println("Escolha a Fonte De Alimentacao: ");
+        estoque.catalogoFontes();
+        opcao = input.nextInt();
+        pedido.addFonte(opcao);
 
-            System.out.println("Escolha o Gabinte: ");
-            estoque.catalogoGabinetes();
-            opcao = input.nextInt();
-            pedido.addGabinete(opcao);
+        System.out.println("Escolha o Gabinte: ");
+        estoque.catalogoGabinetes();
+        opcao = input.nextInt();
+        pedido.addGabinete(opcao);
 
-            System.out.println("Periféricos:");
-            System.out.println("Escolha o mouse:");
-            estoque.catalogoMouses();
-            opcao = input.nextInt();
-            pedido.addMouse(opcao);
+        System.out.println("Periféricos:");
+        System.out.println("Escolha o mouse:");
+        estoque.catalogoMouses();
+        opcao = input.nextInt();
+        pedido.addMouse(opcao);
 
-            System.out.println("Escolha o Teclado: ");
-            estoque.catalogoTeclados();
-            opcao = input.nextInt();
-            pedido.addTeclado(opcao);
+        System.out.println("Escolha o Teclado: ");
+        estoque.catalogoTeclados();
+        opcao = input.nextInt();
+        pedido.addTeclado(opcao);
 
-            System.out.println("Escolha o Fone: ");
-            estoque.catalogoFones();
-            opcao = input.nextInt();
-            pedido.addFone(opcao);
+        System.out.println("Escolha o Fone: ");
+        estoque.catalogoFones();
+        opcao = input.nextInt();
+        pedido.addFone(opcao);
 
-            System.out.println("Escolha o Monitor: ");
+        System.out.println("Escolha o Monitor: ");
+        estoque.catalogoMonitores();
+        for(int i = 0;i<2;i++){
             estoque.catalogoMonitores();
-            for(int i = 0;i<2;i++){
-                estoque.catalogoMonitores();
-                opcao = input.nextInt();
-                pedido.addMonitor(opcao);
-            }
+            opcao = input.nextInt();
+            pedido.addMonitor(opcao);
         }
+    }
         /*
          * Processador
          * Placa Mãe
@@ -106,5 +106,5 @@ public class Main {
          * Perifericos(mouse,teclado,fone,MOnitor)
          */
 
-    }
 }
+
